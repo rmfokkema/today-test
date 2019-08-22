04633e907ec8f0ece30788a57588485203e8b15d
René Fokkema <rmfokkema@gmail.com>
2019-08-22 22:12:34 +0200

    [Init] Random font wallpaper renderer

---
+++ quote-wallpaper/frame-renderer.sh
@@ -0,0 +1,21 @@
+#!/bin/zsh
+backgrounds=(~/Desktop/striped/*)
+randomBackground=$backgrounds[(RANDOM % $#backgrounds + 1)]
+
+IFS=$'\n' fonts=($(< ~/.sh/.fonts.log))
+#for f in $fonts; do echo $f; done
+#exit
+
+randomFont=$fonts[(RANDOM % $#fonts + 1)]
+
+   [ -f ~/Library/Fonts/$randomFont.ttf ] \
+&& randomFont=~/Library/Fonts/$randomFont'.ttf' \
+|| randomFont=~/Library/Fonts/$randomFont'.ttc'
+
+randomRotation=$(( RANDOM % 45 ))
+randomQuote=$(brainy | fold -w 20 -s)
+
+convert $randomBackground -font $randomFont -pointsize 92 -gravity center \
+	-fill black -annotate ${randomRotation}x+0-1 $randomQuote  \
+	-fill white -annotate ${randomRotation}x+0+0 $randomQuote  \
+	~/Desktop/$RANDOM.png
